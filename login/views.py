5db0e1f0c62190d40b42088f717097b00a352269
ali88saluja <x1357990gmail.com>
2019-01-01 19:02:45 +0800

    <message>

--- login/views.py
+++ login/views.py
@@ -5,7 +5,25 @@ from django.http import HttpResponse
 from django.contrib.auth.models import User
 
 # Create your views here.
-	
+def register(request):
+	if request.method == 'POST':
+		name = request.POST['username']
+		password = request.POST['password']
+		email = request.POST.get('email', False)
+
+		try:
+			user = User.objects.get(username=name)
+		except:
+			user = None
+		if user is not None:
+			message = '此使用者已經有人使用'
+		else:
+			user = User.objects.create_user(name, email, password)
+			user.save()
+			message = "註冊成功"
+			return render(request, "index.html", locals())
+	return render(request, 'register.html', locals())
+
 def login(request):
 	if request.method == 'POST':
 		name = request.POST['username']
@@ -15,7 +33,11 @@ def login(request):
 			if user.is_active:
 				auth.login(request,user)
 				return redirect('/website.html/') 
-				
+				message = '登入成功!'
+			else:
+				message = '無此帳號!'
+		else:
+			message = '登入失敗!'
 	return render(request,"login.html",locals()) 
 
 def logout(request):
